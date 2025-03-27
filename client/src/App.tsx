c87a4491d644a1b64edadf69be252141ef56dfa3
Kaloyan Yordanov <kaloyan0102@gmail.com>
2025-03-27 09:36:05 +0200

    Created Private and Auth route hadnler for App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,21 +5,66 @@ import Login from "./components/Login/Login.component";
 import Register from "./components/Register/Register.component";
 import { Profile } from "./components/Profile/Profile.component";
 import { Admin } from "./components/Admin/Admin.component";
+import { Posts } from "./components/Posts/Posts.components";
+import { CreatePost } from "./components/CreatePost/CreatePost.component";
+import { PrivateRoute } from "./components/PrivateRoute/PrivateRoute.component";
+import { AuthRoute } from "./components/AuthRoute/AuthRoute.component";
 
 function App() {
   return (
-    //TODO Add private routes
     <>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/posts" element={<h1>Posts Page</h1>} />
-        <Route path="/messenger" element={<h1>Messenger Page</h1>} />
-        {/* TODO add error hadnling if profile dones't exist - like when entered through url*/}
-        <Route path="/profile/:profileId" element={<Profile />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/admin" element={<Admin />} />
+        <Route path="/posts" element={<Posts />} />
+        <Route 
+          path="/messenger" 
+          element={
+            <PrivateRoute>
+              <h1>Messenger Page</h1>
+            </PrivateRoute>
+          } 
+        />
+        <Route 
+          path="/profile/:profileId" 
+          element={
+            <PrivateRoute>
+              <Profile />
+            </PrivateRoute>
+          } 
+        />
+        <Route 
+          path="/login" 
+          element={
+            <AuthRoute>
+              <Login />
+            </AuthRoute>
+          } 
+        />
+        <Route 
+          path="/register" 
+          element={
+            <AuthRoute>
+              <Register />
+            </AuthRoute>
+          } 
+        />
+        <Route 
+          path="/admin" 
+          element={
+            <PrivateRoute requireAdmin>
+              <Admin />
+            </PrivateRoute>
+          } 
+        />
+        <Route 
+          path="/create-post" 
+          element={
+            <PrivateRoute>
+              <CreatePost />
+            </PrivateRoute>
+          } 
+        />
       </Routes>
     </>
   );
