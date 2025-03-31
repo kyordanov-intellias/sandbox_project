01682ddb7ff2d9e2075bf93f63d12aeff7119cf4
Kaloyan Yordanov <kaloyan0102@gmail.com>
2025-03-31 09:46:02 +0300

    Removed the PostsContext

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,16 +4,13 @@ import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { UserProvider } from "./context/UserContext.tsx";
-import { PostsProvider } from "./context/PostsContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <PostsProvider>
         <UserProvider>
           <App />
         </UserProvider>
-      </PostsProvider>
     </BrowserRouter>
   </StrictMode>
 );
