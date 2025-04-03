f3473b52d408d143cb4df52bb569f75aa35abb86
Kaloyan Yordanov <kaloyanyordanov@kyordanov-mbp.local>
2025-04-03 17:33:21 +0300

    search/client -> adjust the changes into header + styles + installed lodash

--- client/src/components/Header/Header.component.tsx
+++ client/src/components/Header/Header.component.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Home, MessageSquare, Users, User, ShieldUser } from "lucide-react";
 import { useUser } from "../../context/UserContext";
@@ -6,7 +6,31 @@ import "./Header.styles.css";
 
 const Header: FC = () => {
   const { user, logout } = useUser();
-  console.log(user);
+  const [query, setQuery] = useState("");
+  const [results, setResults] = useState<
+    { id: string; firstName: string; lastName: string; profileImage: string }[]
+  >([]);
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  // Add a debounce delay to reduce the number of requests
+  useEffect(() => {
+    const debounceTimeout = setTimeout(() => {
+      if (query.length > 0) {
+        fetch(`http://localhost:4000/users/search?query=${query}&limit=5`)
+          .then((res) => res.json())
+          .then((data) => {
+            setResults(data);
+            setShowDropdown(true);
+          })
+          .catch(() => setShowDropdown(false));
+      } else {
+        setShowDropdown(false);
+      }
+    }, 500); // Wait 500ms after the user stops typing before making the request
+
+    // Clean up the timeout if the query changes before the debounce delay
+    return () => clearTimeout(debounceTimeout);
+  }, [query]);
 
   return (
     <header className="header">
@@ -15,17 +39,48 @@ const Header: FC = () => {
           <Home size={24} />
         </Link>
 
+        <div className="search-container">
+          <input
+            type="text"
+            className="search-input"
+            placeholder="Search users..."
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            onFocus={() => setShowDropdown(true)}
+            onBlur={() => setTimeout(() => setShowDropdown(false), 200)} // Small delay to allow clicks
+          />
+          {showDropdown && results.length > 0 && (
+            <div className="search-dropdown">
+              <ul>
+                {results.map((user) => (
+                  <li key={user.id} onMouseDown={(e) => e.preventDefault()}>
+                    <Link
+                      to={`/profile/${user.id}`}
+                      className="search-result-item"
+                    >
+                      <img
+                        src={user.profileImage || "/default-avatar.png"}
+                        alt={`${user.firstName} ${user.lastName}`}
+                        className="user-avatar"
+                      />
+                      {user.firstName} {user.lastName}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </div>
+
         <nav className="nav-links">
           <Link to="/posts" className="nav-link">
             <Users size={20} />
             Posts
           </Link>
           {!user ? (
-            <>
-              <Link to="/login" className="join-button">
-                Join Now
-              </Link>
-            </>
+            <Link to="/login" className="join-button">
+              Join Now
+            </Link>
           ) : (
             <>
               <Link to="/messenger" className="nav-link">
@@ -42,7 +97,6 @@ const Header: FC = () => {
                   Admin
                 </Link>
               )}
-
               <button className="logout-button" onClick={logout}>
                 Log out
               </button>
