bcd8248d2690415b2ae9b1cd88bf9dff7f9f093f
Kaloyan Yordanov <kaloyanyordanov@kyordanov-mbp.local>
2025-04-08 10:45:45 +0300

    Added lodash for debouncing

--- client/src/components/Header/Header.component.tsx
+++ client/src/components/Header/Header.component.tsx
@@ -2,34 +2,55 @@ import { FC, useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Home, MessageSquare, ShieldUser, BookUser } from "lucide-react";
 import { useUser } from "../../context/UserContext";
+import debounce from "lodash/debounce";
 import "./Header.styles.css";
 
 const Header: FC = () => {
   const { user, logout } = useUser();
   const [query, setQuery] = useState("");
   const [results, setResults] = useState<
-    { id: string; firstName: string; lastName: string; profileImage: string }[]
+    {
+      id: string;
+      authId: string;
+      profileImage: string;
+      firstName: string;
+      lastName: string;
+    }[]
   >([]);
   const [showDropdown, setShowDropdown] = useState(false);
+  const [fadeOut, setFadeOut] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-  // Add a debounce delay to reduce the number of requests
+  const debouncedSearch = debounce((query: string) => {
+    if (query.length === 0) {
+      setShowDropdown(false);
+      setResults([]);
+      return;
+    }
+
+    setLoading(true);
+    
+    fetch(`http://localhost:4000/users/search?query=${query}&limit=5`)
+      .then((res) => res.json())
+      .then((data) => {
+        setResults(data);
+        setShowDropdown(true);
+      })
+      .catch(() => {
+        setResults([]);
+        setShowDropdown(true);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, 500);
+
   useEffect(() => {
-    const debounceTimeout = setTimeout(() => {
-      if (query.length > 0) {
-        fetch(`http://localhost:4000/users/search?query=${query}&limit=5`)
-          .then((res) => res.json())
-          .then((data) => {
-            setResults(data);
-            setShowDropdown(true);
-          })
-          .catch(() => setShowDropdown(false));
-      } else {
-        setShowDropdown(false);
-      }
-    }, 500); // Wait 500ms after the user stops typing before making the request
+    debouncedSearch(query);
 
-    // Clean up the timeout if the query changes before the debounce delay
-    return () => clearTimeout(debounceTimeout);
+    return () => {
+      debouncedSearch.cancel();
+    };
   }, [query]);
 
   return (
@@ -47,27 +68,44 @@ const Header: FC = () => {
             value={query}
             onChange={(e) => setQuery(e.target.value)}
             onFocus={() => setShowDropdown(true)}
-            onBlur={() => setTimeout(() => setShowDropdown(false), 200)} // Small delay to allow clicks
+            onBlur={() => {
+              setFadeOut(true);
+              setTimeout(() => {
+                setShowDropdown(false);
+                setFadeOut(false);
+              }, 200);
+            }}
           />
-          {showDropdown && results.length > 0 && (
-            <div className="search-dropdown">
-              <ul>
-                {results.map((user) => (
-                  <li key={user.id} onMouseDown={(e) => e.preventDefault()}>
-                    <Link
-                      to={`/profile/${user.id}`}
-                      className="search-result-item"
-                    >
-                      <img
-                        src={user.profileImage || "/default-avatar.png"}
-                        alt={`${user.firstName} ${user.lastName}`}
-                        className="user-avatar"
-                      />
-                      {user.firstName} {user.lastName}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
+
+          {showDropdown && (
+            <div
+              className={`search-dropdown ${fadeOut ? "fade-out" : ""} ${
+                results.length === 0 ? "no-results" : ""
+              }`}
+            >
+              {loading ? (
+                <div className="search-loading">Searching...</div>
+              ) : results.length > 0 ? (
+                <ul>
+                  {results.map((user) => (
+                    <li key={user.id} onMouseDown={(e) => e.preventDefault()}>
+                      <Link
+                        to={`/profile/${user.authId}`}
+                        className="search-result-item"
+                      >
+                        <img
+                          src={user.profileImage || "/default-avatar.png"}
+                          alt={`${user.firstName} ${user.lastName}`}
+                          className="user-avatar"
+                        />
+                        {user.firstName} {user.lastName}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              ) : (
+                <div className="no-results-text">No users found</div>
+              )}
             </div>
           )}
         </div>
